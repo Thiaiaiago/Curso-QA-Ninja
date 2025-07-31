833f0465b00fe52f43a7cd5b08612de7bdb7ca10
Thiago Godoy Cunha <nhalykami@gmail.com>
2025-07-31 17:02:39 -0300

    Aula formulario de cadastro 1 parcialmente concluida

--- RockLov/Projeto/features/step_definitions/cadastro_steps.rb
+++ RockLov/Projeto/features/step_definitions/cadastro_steps.rb
@@ -1,9 +1,13 @@
 Dado('que acesso a página de cadastro') do
   visit "http://rocklov-web:3000/signup"
+  sleep 10
 end
 
 Quando('submeto o meu cadastro completo') do
-  pending # Write code here that turns the phrase above into concrete actions
+  find("#fullName").set "Thiago"
+  find("#email").set "thiago@gmail.com"
+  find("#password").set "pwd123"
+  sleep 10
 end
 
 Então('sou redirecionado para o Dashboard') do
